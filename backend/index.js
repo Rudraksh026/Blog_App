74569c0f8c6fe835ed124e2f47d1e5deca3957ac
Rudraksh026 <rudrakshmishra026@gmail.com>
2025-08-24 19:00:59 +0530

    connect backend to frontend

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 const db = require("./database/db");
 
 const corsOptions = {
-    origin:"http://localhost:5173",
+    origin:"https://blogapplication-1zkw.onrender.com/",
     credentials:true
 }
 
